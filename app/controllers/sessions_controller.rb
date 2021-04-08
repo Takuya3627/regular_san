a061eb61ef9424f858428146be4c181acba5a29a
Takuya3627 <tm.0630.ppp@gmail.com>
2021-04-08 03:52:35 +0000

    MVCの作成

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+
+  def after_sign_in_path_for(resource)
+    root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+end
