a70ac56109694c053297ca7550e5d5aff237cf5e
Takuya3627 <tm.0630.ppp@gmail.com>
2021-04-28 09:15:12 +0000

    レイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
+  validates :introduction, length: { maximum: 20 }
 
   has_many :restaurants, dependent: :destroy
   has_many :favorites, dependent: :destroy
