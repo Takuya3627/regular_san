5da98a6607db874375a1d0832ffc52a1aed49919
Takuya3627 <tm.0630.ppp@gmail.com>
2021-04-28 11:55:14 +0000

    ページの上部に戻るボタン実装

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,6 +1,15 @@
 $(function() {
-  $('#top-btn a').on('click',function(event){
-    $('body, html').animate({
+    var topBtn = $('#top-btn a');
+    topBtn.hide();
+    $(window).scroll(function(){
+         if ($(this).scrollTop() > 100) {
+             topBtn.fadeIn(1000);
+         } else {
+             topBtn.fadeOut();
+         }
+    });
+    topBtn.click(function () {
+        $('body, html').animate({
       scrollTop:0
     }, 1000);
     event.preventDefault();
