0b1468ad836b9dc73293623a95e569ef526042b9
Takuya3627 <tm.0630.ppp@gmail.com>
2021-05-07 08:59:02 +0000

    画像レスポンシブ修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "homes#top"
   get "homes/about" => "homes#about"
-  get '/restaurant/category' => "restaurants#category"
 
   resources :restaurants, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    collection do
+      get :category
+    end
     resource :favorites, only: [:create, :destroy]
     resources :restaurant_comments, only: [:index, :create, :destroy]
   end
